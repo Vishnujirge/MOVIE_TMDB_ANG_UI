3a8afc5dd4d3df0ab29b84e4d6b489b2a022e832
dhanashri-bhopale <bhopaledhanashri@gmail.com>
2026-01-07 00:20:52 +0530

    movie card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MovieDashboardComponent } from './shared/component/movie-dashboard/movie-dashboard.component';
+import { MovieCardComponent } from './shared/component/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDashboardComponent,
+    MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
